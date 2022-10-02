1e6e084bcc0f7fa0f4c358e3b349d9162ff65119
sw90lee <sw90lee@naver.com>
2022-10-02 22:41:32 +0900

    authentication microservice 만들기

---
+++ authentication-service/data/models.go
@@ -0,0 +1,269 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"log"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// DB Connect 시 실패 Time
+const dbTimeout = time.Second * 3
+
+var db *sql.DB
+
+// 새로 만들기 기능은 데이터 패키지의 인스턴스를 만드는 데 사용됩니다. 유형을 반환합니다.
+// 이 모델에는 응용 프로그램에서 사용할 수 있는 모든 유형이 포함되어 있습니다.
+func New(dbPool *sql.DB) Models {
+	db = dbPool
+
+	return Models{
+		User: User{},
+	}
+}
+
+// 모델이 이 패키지의 유형입니다. 구성원으로 포함된 모든 모델
+// 이 유형은 애플리케이션 전체에서 어디서나 사용할 수 있습니다.
+// New function에도 모델이 추가되는 경우 App 변수가 사용됩니다.
+type Models struct {
+	User User
+}
+
+// 사용자는 데이터베이스에서 한 명의 사용자를 보유하는 구조입니다.
+type User struct {
+	ID        int       `json:"id"`
+	Email     string    `json:"email"`
+	FirstName string    `json:"first_name,omitempty"`
+	LastName  string    `json:"last_name,omitempty"`
+	Password  string    `json:"-"`
+	Active    int       `json:"active"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+// GetAll은 이름을 기준으로 정렬된 모든 사용자의 조각을 반환합니다.
+func (u *User) GetAll() ([]*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `select id, email, first_name, last_name, password, user_active, created_at, updated_at
+	from users order by last_name`
+
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []*User
+
+	for rows.Next() {
+		var user User
+		err := rows.Scan(
+			&user.ID,
+			&user.Email,
+			&user.FirstName,
+			&user.LastName,
+			&user.Password,
+			&user.Active,
+			&user.CreatedAt,
+			&user.UpdatedAt,
+		)
+		if err != nil {
+			log.Println("Error scanning", err)
+			return nil, err
+		}
+
+		users = append(users, &user)
+	}
+
+	return users, nil
+}
+
+// GetByEmail은 이메일로 한 명의 사용자를 반환합니다.
+func (u *User) GetByEmail(email string) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `select id, email, first_name, last_name, password, user_active, created_at, updated_at from users where email = $1`
+
+	var user User
+	row := db.QueryRowContext(ctx, query, email)
+
+	err := row.Scan(
+		&user.ID,
+		&user.Email,
+		&user.FirstName,
+		&user.LastName,
+		&user.Password,
+		&user.Active,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+// GetOne은 ID로 한 명의 사용자를 반환합니다.
+func (u *User) GetOne(id int) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `select id, email, first_name, last_name, password, user_active, created_at, updated_at from users where id = $1`
+
+	var user User
+	row := db.QueryRowContext(ctx, query, id)
+
+	err := row.Scan(
+		&user.ID,
+		&user.Email,
+		&user.FirstName,
+		&user.LastName,
+		&user.Password,
+		&user.Active,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+// 업데이트는 정보를 사용하여 데이터베이스의 한 사용자를 업데이트합니다.
+// 수신기 u에 저장됨
+func (u *User) Update() error {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	stmt := `update users set
+		email = $1,
+		first_name = $2,
+		last_name = $3,
+		user_active = $4,
+		updated_at = $5
+		where id = $6
+	`
+
+	_, err := db.ExecContext(ctx, stmt,
+		u.Email,
+		u.FirstName,
+		u.LastName,
+		u.Active,
+		time.Now(),
+		u.ID,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// 삭제는 데이터베이스에서 사용자별로 한 명의 사용자를 삭제합니다.
+func (u *User) Delete() error {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	stmt := `delete from users where id = $1`
+
+	_, err := db.ExecContext(ctx, stmt, u.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// 삭제 기준ID는 ID별로 데이터베이스에서 한 명의 사용자를 삭제합니다.
+func (u *User) DeleteByID(id int) error {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	stmt := `delete from users where id = $1`
+
+	_, err := db.ExecContext(ctx, stmt, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Insert는 데이터베이스에 새 사용자를 삽입하고, 새로 삽입된 행의 ID를 반환합니다.
+func (u *User) Insert(user User) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), 12)
+	if err != nil {
+		return 0, err
+	}
+
+	var newID int
+	stmt := `insert into users (email, first_name, last_name, password, user_active, created_at, updated_at)
+		values ($1, $2, $3, $4, $5, $6, $7) returning id`
+
+	err = db.QueryRowContext(ctx, stmt,
+		user.Email,
+		user.FirstName,
+		user.LastName,
+		hashedPassword,
+		user.Active,
+		time.Now(),
+		time.Now(),
+	).Scan(&newID)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return newID, nil
+}
+
+// 암호 재설정은 사용자의 암호를 변경하는 데 사용할 방법입니다.
+func (u *User) ResetPassword(password string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	stmt := `update users set password = $1 where id = $2`
+	_, err = db.ExecContext(ctx, stmt, hashedPassword, u.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// PasswordMatches는 Go의 bcrypt 패키지를 사용하여 사용자 제공 암호를 비교합니다.
+// 지정된 사용자에 대해 데이터베이스에 저장한 해시를 사용합니다. 암호의 경우
+// 그리고 해시 일치, 우리는 true를 반환하고, 그렇지 않으면 false를 반환한다.
+func (u *User) PasswordMatches(plainText string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(plainText))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			// invalid password
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+
+	return true, nil
+}
--- broker-service/broker-service.dockerfile
+++ broker-service/broker-service.dockerfile
@@ -1,22 +1,7 @@
-# BASE IMAGE go
-FROM golang:1.19.1-alpine as builder
-
-RUN mkdir /app
-
-COPY . /app
-
-WORKDIR /app
-
-RUN CGO_ENABLED=0 go build -o brokerApp ./cmd/api
-
-RUN chmod +x /app/brokerApp
-
-# build a tiny docker image
-
 FROM alpine:latest
 
 RUN mkdir /app
 
-COPY --from=builder /app/brokerApp /app
+COPY brokerApp /app
 
 CMD ["/app/brokerApp"]

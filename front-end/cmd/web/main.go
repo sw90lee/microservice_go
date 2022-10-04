7f0e3fe57741ca4c06b617c38d3d3d4738cf48ae
sw90lee <sw90lee@naver.com>
2022-10-04 23:14:14 +0900

    auth2

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -22,13 +22,13 @@ func main() {
 func render(w http.ResponseWriter, t string) {
 
 	partials := []string{
-		".cmd/web/templates/base.layout.gohtml",
-		".cmd/web/templates/header.partial.gohtml",
-		".cmd/web/templates/footer.partial.gohtml",
+		"templates/base.layout.gohtml",
+		"templates/header.partial.gohtml",
+		"templates/footer.partial.gohtml",
 	}
 
 	var templateSlice []string
-	templateSlice = append(templateSlice, fmt.Sprintf(".cmd/web/templates/%s", t))
+	templateSlice = append(templateSlice, fmt.Sprintf("templates/%s", t))
 
 	for _, x := range partials {
 		templateSlice = append(templateSlice, x)
